176e5eaadc8ab65d58b622dda569e2cc93b3f248
Geert Bevin <gbevin@uwyn.com>
2023-01-02 11:53:00 -0500

    Added max-age 0 in prevent caching helper

--- lib/src/main/java/rife/tools/ServletUtils.java
+++ lib/src/main/java/rife/tools/ServletUtils.java
@@ -12,6 +12,7 @@ public final class ServletUtils {
     }
 
     public static void preventCaching(Response response) {
+        response.addHeader("Cache-Control", "max-age=0");           // HTTP/1.0
         response.addHeader("Cache-Control", "no-cache");            // HTTP/1.1
         response.addHeader("Cache-Control", "no-store");            // HTTP/1.1
         response.addHeader("Cache-Control", "must-revalidate");     // HTTP/1.1
