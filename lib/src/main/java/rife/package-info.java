3048dffa3efe1536587c2d48679fbf77fc5c87ee
Geert Bevin <gbevin@uwyn.com>
2023-01-17 08:49:44 -0500

    Added root package info file

---
+++ lib/src/main/java/rife/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+
+/**
+ * Full-stack framework to effortlessly create web applications with modern Java.
+ * @since 1.0
+ */
+package rife;
