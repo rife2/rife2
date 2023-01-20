927f9c7cebed74ac3f315ab28295252297720f4b
Geert Bevin <gbevin@uwyn.com>
2023-01-20 11:48:19 -0500

    Workflow example cleanup

--- app/src/main/java/rife/HelloWorkflow.java
+++ app/src/main/java/rife/HelloWorkflow.java
@@ -8,7 +8,7 @@ import rife.engine.Server;
 import rife.engine.Site;
 import rife.workflow.*;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import static rife.HelloWorkflow.WorkType.*;
 
 public class HelloWorkflow extends Site {
     enum WorkType {
@@ -20,7 +20,7 @@ public class HelloWorkflow extends Site {
     public class EmailWork implements Work {
         public void execute(Workflow workflow) {
             while (true) {
-                var event = pauseForEvent(WorkType.EMAIL);
+                var event = pauseForEvent(EMAIL);
 
                 System.out.println("Sending email " + event.getData());
             }
@@ -30,25 +30,26 @@ public class HelloWorkflow extends Site {
     public class PaymentWork implements Work {
         public void execute(Workflow workflow) {
             while (true) {
-                var event = pauseForEvent(WorkType.PAYMENT);
+                var event = pauseForEvent(PAYMENT);
 
                 System.out.println("Waiting for confirmation " + event.getData());
                 workflow.start(new ConfirmationWork((String)event.getData()));
-                workflow.trigger(WorkType.EMAIL, event.getData());
+                workflow.trigger(EMAIL, event.getData());
             }
         }
     }
 
     public class ConfirmationWork implements Work {
-        private final String expectedEmail_;
-        public ConfirmationWork(String email) {
-            expectedEmail_ = email;
+        private final String expectedAccount_;
+        public ConfirmationWork(String account) {
+            expectedAccount_ = account;
         }
         public void execute(Workflow workflow) {
             Event event;
             do {
-                event = pauseForEvent(WorkType.CONFIRMATION);
-            } while (!event.getData().equals(expectedEmail_));
+                event = pauseForEvent(CONFIRMATION);
+            } while (event.getData() == null ||
+                     !event.getData().equals(expectedAccount_));
 
             System.out.println("Sending payment " + event.getData());
         }
@@ -57,9 +58,9 @@ public class HelloWorkflow extends Site {
     public void setup() {
         workflow.start(new EmailWork()).start(new PaymentWork());
 
-        get("/email", c -> workflow.trigger(WorkType.EMAIL, c.parameter("account")));
-        get("/payment", c -> workflow.trigger(WorkType.PAYMENT, c.parameter("account")));
-        get("/confirm", c -> workflow.trigger(WorkType.CONFIRMATION, c.parameter("account")));
+        get("/email", c -> workflow.trigger(EMAIL, c.parameter("account")));
+        get("/payment", c -> workflow.trigger(PAYMENT, c.parameter("account")));
+        get("/confirm", c -> workflow.inform(CONFIRMATION, c.parameter("account")));
     }
 
     public static void main(String[] args) {
