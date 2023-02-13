c51f88b3923fc9145f424567742a0199aba89067
Geert Bevin <gbevin@uwyn.com>
2023-02-13 17:03:11 -0500

    Updated version to 1.2.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     signing
 }
 
-val rifeVersion by rootProject.extra { "1.2.0-SNAPSHOT" }
+val rifeVersion by rootProject.extra { "1.2.0" }
 var rifeAgentName: String = "rife2-$rifeVersion-agent"
 val rifeAgentJar by rootProject.extra { "$rifeAgentName.jar" }
 var rifeAgentContinuationsName: String = "rife2-$rifeVersion-agent-continuations"
