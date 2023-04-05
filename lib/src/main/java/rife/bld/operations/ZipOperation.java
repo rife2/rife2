ebc8d2018bb0a4e2449fd7462ea2045a35b44b8c
Geert Bevin <gbevin@uwyn.com>
2023-04-05 18:37:30 -0400

    Added zip operation

---
+++ lib/src/main/java/rife/bld/operations/ZipOperation.java
@@ -0,0 +1,335 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+package rife.bld.operations;
+
+import rife.bld.NamedFile;
+import rife.tools.FileUtils;
+import rife.tools.StringUtils;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import java.util.regex.Pattern;
+
+/**
+ * Creates a zip archive of the provided sources and directories.
+ *
+ * @author Geert Bevin (gbevin[remove] at uwyn dot com)
+ * @since 1.5.18
+ */
+public class ZipOperation extends AbstractOperation<ZipOperation> {
+    private final List<File> sourceDirectories_ = new ArrayList<>();
+    private final List<NamedFile> sourceFiles_ = new ArrayList<>();
+    private File destinationDirectory_;
+    private String destinationFileName_;
+    private final List<Pattern> included_ = new ArrayList<>();
+    private final List<Pattern> excluded_ = new ArrayList<>();
+
+    private final byte[] buffer_ = new byte[1024];
+
+    /**
+     * Performs the zip operation.
+     *
+     * @throws IOException when an exception occurred during the zip creation process
+     * @since  1.5.18
+     */
+    public void execute()
+    throws IOException {
+        executeCreateDestinationDirectory();
+        executeCreateZipArchive();
+
+        if (!silent()) {
+            System.out.println("The zip archive was created at '" + new File(destinationDirectory(), destinationFileName()) + "'");
+        }
+    }
+
+    /**
+     * Part of the {@link #execute} operation, create the destination directory.
+     *
+     * @since 1.5.18
+     */
+    protected void executeCreateDestinationDirectory() {
+        destinationDirectory().mkdirs();
+    }
+
+    /**
+     * Part of the {@link #execute} operation, create the zip archive.
+     *
+     * @since 1.5.18
+     */
+    protected void executeCreateZipArchive()
+    throws IOException {
+        var out_file = new File(destinationDirectory(), destinationFileName());
+        try (var zip = new ZipOutputStream(new FileOutputStream(out_file))) {
+            for (var source_dir : sourceDirectories()) {
+                for (var file_name : FileUtils.getFileList(source_dir)) {
+                    var file = new File(source_dir, file_name);
+                    if (StringUtils.filter(file.getAbsolutePath(), included(), excluded(), false)) {
+                        executeAddFileToZip(zip, new NamedFile(file_name, file));
+                    }
+                }
+            }
+            for (var source_file : sourceFiles()) {
+                if (StringUtils.filter(source_file.file().getAbsolutePath(), included(), excluded(), false)) {
+                    executeAddFileToZip(zip, source_file);
+                }
+            }
+            zip.flush();
+        }
+    }
+
+    /**
+     * Part of the {@link #execute} operation, add a single file to the zip archive.
+     *
+     * @since  1.5.18
+     */
+    protected void executeAddFileToZip(ZipOutputStream zip, NamedFile file)
+    throws IOException {
+        var entry = new ZipEntry(file.name().replace('\\', '/'));
+        entry.setTime(file.file().lastModified());
+        zip.putNextEntry(entry);
+
+        try (var in = new BufferedInputStream(new FileInputStream(file.file()))) {
+            int count;
+            while ((count = in.read(buffer_)) != -1) {
+                zip.write(buffer_, 0, count);
+            }
+            zip.closeEntry();
+        }
+    }
+
+    /**
+     * Provides source directories that will be used for the zip archive creation.
+     *
+     * @param directories source directories
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation sourceDirectories(File... directories) {
+        sourceDirectories_.addAll(List.of(directories));
+        return this;
+    }
+
+    /**
+     * Provides a list of source directories that will be used for the zip archive creation.
+     * <p>
+     * A copy will be created to allow this list to be independently modifiable.
+     *
+     * @param directories a list of source directories
+     * @return this operation instance
+     * @since  1.5.18
+     */
+    public ZipOperation sourceDirectories(List<File> directories) {
+        sourceDirectories_.addAll(directories);
+        return this;
+    }
+
+    /**
+     * Provides source files that will be used for the zip archive creation.
+     *
+     * @param files source files
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation sourceFiles(NamedFile... files) {
+        sourceFiles_.addAll(List.of(files));
+        return this;
+    }
+
+    /**
+     * Provides a list of source files that will be used for the zip archive creation.
+     * <p>
+     * A copy will be created to allow this list to be independently modifiable.
+     *
+     * @param files a list of source files
+     * @return this operation instance
+     * @since  1.5.18
+     */
+    public ZipOperation sourceFiles(List<NamedFile> files) {
+        sourceFiles_.addAll(files);
+        return this;
+    }
+
+    /**
+     * Provides the destination directory in which the zip archive will be created.
+     *
+     * @param directory the zip destination directory
+     * @return this operation instance
+     * @since  1.5.18
+     */
+    public ZipOperation destinationDirectory(File directory) {
+        destinationDirectory_ = directory;
+        return this;
+    }
+
+    /**
+     * Provides the destination file name that will be used for the zip archive creation.
+     *
+     * @param name the zip archive destination file name
+     * @return this operation instance
+     * @since  1.5.18
+     */
+    public ZipOperation destinationFileName(String name) {
+        destinationFileName_ = name;
+        return this;
+    }
+
+    /**
+     * Provides regex patterns that will be found to determine which files
+     * will be included in the javadoc generation.
+     *
+     * @param included inclusion patterns
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation included(String... included) {
+        included_.addAll(Arrays.stream(included).map(Pattern::compile).toList());
+        return this;
+    }
+
+    /**
+     * Provides patterns that will be found to determine which files
+     * will be included in the zip archive.
+     *
+     * @param included inclusion patterns
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation included(Pattern... included) {
+        included_.addAll(List.of(included));
+        return this;
+    }
+
+    /**
+     * Provides a list of patterns that will be found to determine which files
+     * will be included in the zip archive.
+     * <p>
+     * A copy will be created to allow this list to be independently modifiable.
+     *
+     * @param included a list of inclusion patterns
+     * @return this operation instance
+     * @since  1.5.18
+     */
+    public ZipOperation included(List<Pattern> included) {
+        included_.addAll(included);
+        return this;
+    }
+
+    /**
+     * Provides regex patterns that will be found to determine which files
+     * will be excluded from the javadoc generation.
+     *
+     * @param excluded exclusion patterns
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation excluded(String... excluded) {
+        excluded_.addAll(Arrays.stream(excluded).map(Pattern::compile).toList());
+        return this;
+    }
+
+    /**
+     * Provides patterns that will be found to determine which files
+     * will be excluded from the zip archive.
+     *
+     * @param excluded exclusion patterns
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation excluded(Pattern... excluded) {
+        excluded_.addAll(List.of(excluded));
+        return this;
+    }
+
+    /**
+     * Provides a list of patterns that will be found to determine which files
+     * will be excluded from the zip archive.
+     * <p>
+     * A copy will be created to allow this list to be independently modifiable.
+     *
+     * @param excluded a list of exclusion patterns
+     * @return this operation instance
+     * @since 1.5.18
+     */
+    public ZipOperation excluded(List<Pattern> excluded) {
+        excluded_.addAll(excluded);
+        return this;
+    }
+
+    /**
+     * Retrieves the list of source directories that will be used for the
+     * zip archive creation.
+     * <p>
+     * This is a modifiable list that can be retrieved and changed.
+     *
+     * @return the zip archive's source directories
+     * @since 1.5.18
+     */
+    public List<File> sourceDirectories() {
+        return sourceDirectories_;
+    }
+
+    /**
+     * Retrieves the list of source files that will be used for the
+     * zip archive creation.
+     * <p>
+     * This is a modifiable list that can be retrieved and changed.
+     *
+     * @return the zip archive's source files
+     * @since 1.5.18
+     */
+    public List<NamedFile> sourceFiles() {
+        return sourceFiles_;
+    }
+
+    /**
+     * Retrieves the destination directory in which the zip archive will
+     * be created.
+     *
+     * @return the zip archive's destination directory
+     * @since 1.5.18
+     */
+    public File destinationDirectory() {
+        return destinationDirectory_;
+    }
+
+    /**
+     * Retrieves the destination file name that will be used for the zip
+     * archive creation.
+     *
+     * @return the zip archive's destination file name
+     * @since 1.5.18
+     */
+    public String destinationFileName() {
+        return destinationFileName_;
+    }
+
+    /**
+     * Retrieves the list of patterns that will be evaluated to determine which files
+     * will be included in the zip archive.
+     * <p>
+     * This is a modifiable list that can be retrieved and changed.
+     *
+     * @return the zip's archive's inclusion patterns
+     * @since 1.5.18
+     */
+    public List<Pattern> included() {
+        return included_;
+    }
+
+    /**
+     * Retrieves the list of patterns that will be evaluated to determine which files
+     * will be excluded the zip archive.
+     * <p>
+     * This is a modifiable list that can be retrieved and changed.
+     *
+     * @return the zip's archive's exclusion patterns
+     * @since 1.5.18
+     */
+    public List<Pattern> excluded() {
+        return excluded_;
+    }
+}
---
+++ lib/src/test/java/rife/bld/operations/TestZipOperation.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+package rife.bld.operations;
+
+import org.junit.jupiter.api.Test;
+import rife.bld.NamedFile;
+import rife.tools.FileUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.zip.ZipFile;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestZipOperation {
+    @Test
+    void testInstantiation() {
+        var operation = new ZipOperation();
+        assertTrue(operation.sourceDirectories().isEmpty());
+        assertTrue(operation.sourceFiles().isEmpty());
+        assertNull(operation.destinationDirectory());
+        assertNull(operation.destinationFileName());
+        assertTrue(operation.included().isEmpty());
+        assertTrue(operation.excluded().isEmpty());
+    }
+
+    @Test
+    void testPopulation() {
+        var source_directory1 = new File("sourceDirectory1");
+        var source_directory2 = new File("sourceDirectory2");
+        var source_file1 = new NamedFile("sourceFile1", new File("sourceFile1"));
+        var source_file2 = new NamedFile("sourceFile2", new File("sourceFile2"));
+        var destination_directory = new File("destinationDirectory");
+        var destination_fileName = "destinationFileName";
+        var included1 = Pattern.compile("included1");
+        var included2 = Pattern.compile("included2");
+        var excluded1 = Pattern.compile("excluded1");
+        var excluded2 = Pattern.compile("excluded2");
+
+        var operation1 = new ZipOperation()
+            .sourceDirectories(List.of(source_directory1, source_directory2))
+            .sourceFiles(List.of(source_file1, source_file2))
+            .destinationDirectory(destination_directory)
+            .destinationFileName(destination_fileName)
+            .included(List.of(included1, included2))
+            .excluded(List.of(excluded1, excluded2));
+
+        assertTrue(operation1.sourceDirectories().contains(source_directory1));
+        assertTrue(operation1.sourceDirectories().contains(source_directory2));
+        assertTrue(operation1.sourceFiles().contains(source_file1));
+        assertTrue(operation1.sourceFiles().contains(source_file2));
+        assertEquals(destination_directory, operation1.destinationDirectory());
+        assertEquals(destination_fileName, operation1.destinationFileName());
+        assertTrue(operation1.included().contains(included1));
+        assertTrue(operation1.included().contains(included2));
+        assertTrue(operation1.excluded().contains(excluded1));
+        assertTrue(operation1.excluded().contains(excluded2));
+
+        var operation2 = new ZipOperation()
+            .destinationDirectory(destination_directory)
+            .destinationFileName(destination_fileName);
+        operation2.sourceDirectories().add(source_directory1);
+        operation2.sourceDirectories().add(source_directory2);
+        operation2.sourceFiles().add(source_file1);
+        operation2.sourceFiles().add(source_file2);
+        operation2.included().add(included1);
+        operation2.included().add(included2);
+        operation2.excluded().add(excluded1);
+        operation2.excluded().add(excluded2);
+
+        assertTrue(operation2.sourceDirectories().contains(source_directory1));
+        assertTrue(operation2.sourceDirectories().contains(source_directory2));
+        assertTrue(operation2.sourceFiles().contains(source_file1));
+        assertTrue(operation2.sourceFiles().contains(source_file2));
+        assertEquals(destination_directory, operation2.destinationDirectory());
+        assertEquals(destination_fileName, operation2.destinationFileName());
+        assertTrue(operation2.included().contains(included1));
+        assertTrue(operation2.included().contains(included2));
+        assertTrue(operation2.excluded().contains(excluded1));
+        assertTrue(operation2.excluded().contains(excluded2));
+
+        var operation3 = new ZipOperation()
+            .sourceDirectories(source_directory1, source_directory2)
+            .sourceFiles(source_file1, source_file2)
+            .destinationDirectory(destination_directory)
+            .destinationFileName(destination_fileName)
+            .included(included1, included2)
+            .excluded(excluded1, excluded2);
+
+        assertTrue(operation3.sourceDirectories().contains(source_directory1));
+        assertTrue(operation3.sourceDirectories().contains(source_directory2));
+        assertTrue(operation3.sourceFiles().contains(source_file1));
+        assertTrue(operation3.sourceFiles().contains(source_file2));
+        assertEquals(destination_directory, operation3.destinationDirectory());
+        assertEquals(destination_fileName, operation3.destinationFileName());
+        assertTrue(operation3.included().contains(included1));
+        assertTrue(operation3.included().contains(included2));
+        assertTrue(operation3.excluded().contains(excluded1));
+        assertTrue(operation3.excluded().contains(excluded2));
+    }
+
+    @Test
+    void testExecute()
+    throws Exception {
+        var tmp = Files.createTempDirectory("test").toFile();
+        try {
+            var source_dir = new File(tmp, "source");
+            var destination_dir = new File(tmp, "destination");
+            var destination_name = "archive.zip";
+
+            source_dir.mkdirs();
+            FileUtils.writeString("source1", new File(source_dir, "source1.text"));
+            FileUtils.writeString("source2", new File(source_dir, "source2.text"));
+            FileUtils.writeString("source3", new File(source_dir, "source3.text"));
+            FileUtils.writeString("source4", new File(source_dir, "source4.txt"));
+            var source5 = new File(tmp, "source5.text");
+            var source6 = new File(tmp, "source6.text");
+            FileUtils.writeString("source5", source5);
+            FileUtils.writeString("source6", source6);
+
+            new ZipOperation()
+                .sourceDirectories(List.of(source_dir))
+                .sourceFiles(List.of(
+                    new NamedFile("src5.txt", source5),
+                    new NamedFile("src6.txt", source6)))
+                .destinationDirectory(destination_dir)
+                .destinationFileName(destination_name)
+                .included("source.*\\.text")
+                .excluded("source5.*")
+                .execute();
+
+            var zip_archive = new File(destination_dir, destination_name);
+            assertTrue(zip_archive.exists());
+
+            var content = new StringBuilder();
+            try (var zip = new ZipFile(zip_archive)) {
+                var e = zip.entries();
+                while (e.hasMoreElements()) {
+                    var zip_entry = e.nextElement();
+                    content.append(zip_entry.getName());
+                    content.append("\n");
+                }
+            }
+
+            assertEquals("""
+                source1.text
+                source2.text
+                source3.text
+                src6.txt
+                """, content.toString());
+        } finally {
+            FileUtils.deleteDirectory(tmp);
+        }
+    }
+
+}
