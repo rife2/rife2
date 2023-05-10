624ae73099653733be4aaa8d955172469bcd0a64
Geert Bevin <gbevin@uwyn.com>
2023-05-10 10:15:26 -0400

    Javadocs

--- src/main/java/rife/cmf/MimeTypeFormatter.java
+++ src/main/java/rife/cmf/MimeTypeFormatter.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
 package rife.cmf;
 
 import rife.cmf.format.*;
 
+/**
+ * Returns appropriate formatter instances for supported CMF mime types.
+ *
+ * @author Geert Bevin (gbevin[remove] at uwyn dot com)
+ * @since 1.7
+ */
 public final class MimeTypeFormatter {
     private MimeTypeFormatter() {
     }
 
+    /**
+     * Returns an instance of the formatter for the provided mime type.
+     *
+     * @param mimeType the mime type for which a formatter should be instantiated
+     * @return and appropriate formatter instance; or {@code null} if no formatter could be created
+     * @since 1.7
+     */
     public static Formatter getFormatter(MimeType mimeType) {
         switch (mimeType.getIdentifier()) {
             case MimeType.APPLICATION_XHTML_IDENTIFIER -> {
--- src/main/java/rife/cmf/MimeTypeValidation.java
+++ src/main/java/rife/cmf/MimeTypeValidation.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
 package rife.cmf;
 
 import rife.cmf.validation.CmfPropertyValidationRule;
@@ -5,10 +9,24 @@ import rife.cmf.validation.SupportedImage;
 import rife.cmf.validation.SupportedXhtml;
 import rife.validation.ConstrainedProperty;
 
+/**
+ * Returns appropriate validation rules for supported CMF mime types.
+ *
+ * @author Geert Bevin (gbevin[remove] at uwyn dot com)
+ * @since 1.7
+ */
 public final class MimeTypeValidation {
     private MimeTypeValidation() {
     }
 
+    /**
+     * Constructs and returns a CMF-specific validation rule that is able to validate data for this mime type.
+     *
+     * @param mimeType the mime type for which the validation rule has to be built
+     * @param constrainedProperty an instance of the property for which the validation rule has to be built
+     * @return an instance of the validation rule; or {@code null} of no validation rule could be created
+     * @since 1.7
+     */
     public static CmfPropertyValidationRule getValidationRule(MimeType mimeType, ConstrainedProperty constrainedProperty) {
         switch (mimeType.getIdentifier()) {
             case MimeType.APPLICATION_XHTML_IDENTIFIER -> {
